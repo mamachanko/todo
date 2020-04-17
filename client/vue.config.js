fa68473e37ab37efd0ac4d9e217825f66ed64129
mamachanko <sugardubz@gmail.com>
2020-04-17 17:04:12 +0200

    integrate e2e into ship script

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://localhost:8080/",
+        target: process.env.API_BASE_URL || "http://localhost:8080/",
       },
     },
   },
--- scripts/ship.sh
+++ scripts/ship.sh
@@ -5,13 +5,31 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 echo ""
-echo "👷🏻‍♀️let's ship"
+echo "👷🏻‍♀️ let's build"
 echo ""
 
 ./build-client.sh
 ./build-api.sh
 
+
+echo ""
+echo "🧪️ let's e2e"
+echo ""
+
+export API_BASE_URL=http://api:8080
+
+docker-compose down --volumes --remove-orphans
+docker-compose up --detach
+docker-compose exec client /scripts/wait ${API_BASE_URL/http:\/\//} --timeout=120 -- echo good to go
+./e2e.sh
+docker-compose down --volumes --remove-orphans
+
+echo ""
+echo "🆙 let's push"
+echo ""
+
 git push
+
 
 echo ""
 echo "🚢 successfully shipped"
